c02cb1920b0257b94aa55a0250b59d18efdd807d
keani-julian <julian_keani01@student.smc.edu>
2026-07-30 18:34:59 -0700

    gitig weather.json

--- json-playground/app/Http/Controllers/WeatherController.php
+++ json-playground/app/Http/Controllers/WeatherController.php
@@ -3,8 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Storage;
 
 class WeatherController extends Controller
 {
-    //
+    public function index()
+    {
+        $json = Storage::get('weather.json');
+        $weatherData = json_decode($json, true);
+
+        return view('weather.index', ['weather' => $weatherData]);
+    }
 }
